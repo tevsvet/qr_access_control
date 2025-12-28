9f9bd6c280b6467914ceb7928dd06b17ff356b21
tevsvet <naumkina.alina601@gmail.com>
2025-12-28 21:30:52 +0300

    Initial commit

---
+++ src/main/java/com/program/qr_access_control/QrAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.program.qr_access_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrAccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/program/qr_access_control/QrAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.qr_access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
